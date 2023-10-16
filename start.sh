13555d44dd3e6b494dcc7c51b115b333e0bf3b6a
UnknownBotz <145199654+UnknownBotz@users.noreply.github.com>
2023-10-16 19:27:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Rohaniscoder/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/UnknownBotz/AutoFilterWithStream.git /AutoFilterWithStream
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /AutoFilterWithStream
 fi
-cd /DQ-The-File-Donor
+cd /AutoFilterWithStream
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting AutoFilterWithStream...."
 python3 bot.py
